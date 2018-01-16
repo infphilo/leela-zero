433a6b7da19dae4a6dfa4f92513f695760ecf514
infphilo <infphilo@gmail.com>
2018-01-16 07:09:26 -0600

    .

--- training/tf/run_play_GPU.sh
+++ training/tf/run_play_GPU.sh
@@ -3,25 +3,9 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
-#SBATCH --partition=GPUv1
+#SBATCH --partition=GPU
 #SBATCH --gres=gpu:1
 #SBATCH --workdir=/home2/s175443/work/image_analysis/leela-zero/autogtp
-/home2/s175443/work/image_analysis/leela-zero/autogtp/autogtp -k test &
-/home2/s175443/work/image_analysis/leela-zero/autogtp/autogtp -k test &
-/home2/s175443/work/image_analysis/leela-zero/autogtp/autogtp -k test &
-/home2/s175443/work/image_analysis/leela-zero/autogtp/autogtp -k test &
-/home2/s175443/work/image_analysis/leela-zero/autogtp/autogtp -k test &
-/home2/s175443/work/image_analysis/leela-zero/autogtp/autogtp -k test &
-/home2/s175443/work/image_analysis/leela-zero/autogtp/autogtp -k test &
-/home2/s175443/work/image_analysis/leela-zero/autogtp/autogtp -k test &
-/home2/s175443/work/image_analysis/leela-zero/autogtp/autogtp -k test &
-/home2/s175443/work/image_analysis/leela-zero/autogtp/autogtp -k test &
-/home2/s175443/work/image_analysis/leela-zero/autogtp/autogtp -k test &
-/home2/s175443/work/image_analysis/leela-zero/autogtp/autogtp -k test &
-/home2/s175443/work/image_analysis/leela-zero/autogtp/autogtp -k test &
-/home2/s175443/work/image_analysis/leela-zero/autogtp/autogtp -k test &
-/home2/s175443/work/image_analysis/leela-zero/autogtp/autogtp -k test &
-/home2/s175443/work/image_analysis/leela-zero/autogtp/autogtp -k test &
 /home2/s175443/work/image_analysis/leela-zero/autogtp/autogtp -k test &
 /home2/s175443/work/image_analysis/leela-zero/autogtp/autogtp -k test &
 /home2/s175443/work/image_analysis/leela-zero/autogtp/autogtp -k test &
