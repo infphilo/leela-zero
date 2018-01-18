ea303812c7066617123261699c89970d34b0ce4e
Daehwan Kim <infphilo@gmail.com>
2018-01-18 10:56:31 -0600

    .

--- extract_games.py
+++ extract_games.py
@@ -4,6 +4,7 @@ from argparse import ArgumentParser, FileType
 
 def extract_games(game_file,
                   num_games,
+                  num_stones,
                   verbose):
     unique_games, trans_unique_games, games = {}, {}, []
     for line in game_file:
@@ -13,49 +14,49 @@ def extract_games(game_file,
             play = ""
         else:
             content.append(line)
-            if line.startswith(';'):
-                play += line
-                if line.endswith(')'):
-                    content = '\n'.join(content)
-                    games.append(content)
-                    play = play[:-1]
-                    play = play.split(';')[1:]
+            play += line
+            if line.endswith(')'):
+                content = '\n'.join(content)
+                games.append(content)
+                play = play[:-1]
+                play = play.split(';')[1:]
+                play = play[:num_stones]
 
-                    def transform(play, xt, yt):
-                        play2 = []
-                        for p in play:
-                            stone, _, x, y, _ = p
-                            x, y = ord(x) - ord('a'), ord(y) - ord('a')
-                            assert x >= 0 and x < 19 and y >= 0 and y < 19
-                            if xt == 1:
-                                x = 18 - x
-                            if yt == 1:
-                                y = 18 - x
-                            x, y = chr(x + ord('a')), chr(y + ord('a'))
-                            play2.append("%s[%s%s]" % (stone, x, y))
-                        return play2
+                def transform(play, xt, yt):
+                    play2 = []
+                    for p in play:
+                        stone, _, x, y, _ = p
+                        x, y = ord(x) - ord('a'), ord(y) - ord('a')
+                        assert x >= 0 and x < 19 and y >= 0 and y < 19
+                        if xt == 1:
+                            x = 18 - x
+                        if yt == 1:
+                            y = 18 - y
+                        x, y = chr(x + ord('a')), chr(y + ord('a'))
+                        play2.append("%s[%s%s]" % (stone, x, y))
+                    return play2
 
-                    play2 = transform(play, 1, 0)
-                    play3 = transform(play, 0, 1)
-                    play4 = transform(play, 1, 1)
-                    
-                    play = ''.join(play)
-                    play2 = ''.join(play2)
-                    play3 = ''.join(play3)
-                    play4 = ''.join(play4)
+                play2 = transform(play, 1, 0)
+                play3 = transform(play, 0, 1)
+                play4 = transform(play, 1, 1)
 
-                    if play not in unique_games:
-                        unique_games[play] = [len(games) - 1]
-                    else:
-                        unique_games[play].append(len(games) - 1)
+                play = ''.join(play)
+                play2 = ''.join(play2)
+                play3 = ''.join(play3)
+                play4 = ''.join(play4)
 
-                    plays = [play, play2, play3, play4]
-                    plays.sort()
-                    play = plays[0]
-                    if play not in trans_unique_games:
-                        trans_unique_games[play] = [len(games) - 1]
-                    else:
-                        trans_unique_games[play].append(len(games) - 1)
+                if play not in unique_games:
+                    unique_games[play] = [len(games) - 1]
+                else:
+                    unique_games[play].append(len(games) - 1)
+
+                plays = [play, play2, play3, play4]
+                plays.sort()
+                play = plays[0]
+                if play not in trans_unique_games:
+                    trans_unique_games[play] = [len(games) - 1]
+                else:
+                    trans_unique_games[play].append(len(games) - 1)
 
     print >> sys.stderr, "# of games: %d, # of unique games: %d, # of transformed unique games: %d" % (len(games), len(unique_games), len(trans_unique_games))
 
@@ -68,7 +69,7 @@ def extract_games(game_file,
         assert game_key in unique_games
         game_num = unique_games[game_key][0]
         print games[game_num]
-                    
+
 
 if __name__ == '__main__':
     parser = ArgumentParser(
@@ -82,6 +83,11 @@ if __name__ == '__main__':
                         type=int,
                         default=0,
                         help='number of games to be randomly chosen and reported')
+    parser.add_argument('--num-stones',
+                        dest='num_stones',
+                        type=int,
+                        default=sys.maxint,
+                        help='number of games to be randomly chosen and reported')
     parser.add_argument('-v', '--verbose',
                         dest='verbose',
                         action='store_true',
@@ -93,5 +99,6 @@ if __name__ == '__main__':
         exit(1)
     extract_games(args.game_file,
                   args.num_games,
+                  args.num_stones,
                   args.verbose)
     
