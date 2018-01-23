7b7c65cfd5470eca00b92a9421abc0da81b08a57
infphilo <infphilo@gmail.com>
2018-01-23 15:04:07 -0600

    .

--- training/tf/run_train.sh
+++ training/tf/run_train.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=test
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=16G
+#SBATCH --mem=64G
 #SBATCH --partition=GPUv1
 #SBATCH --gres=gpu:1
 #SBATCH --workdir=/home2/s175443/work/image_analysis/leela-zero/training/tf
