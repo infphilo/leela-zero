b4c71b6bf4fabdbd0ec039c992e371f7eced4706
Daehwan Kim <infphilo@gmail.com>
2018-02-21 11:23:50 -0600

    .

--- extract_games.py
+++ extract_games.py
@@ -2,121 +2,152 @@
 import sys, random
 from argparse import ArgumentParser, FileType
 
+def draw_game(play):
+    game = [[0 for _ in range(19)] for _ in range(19)]
+    for p in play:
+        stone, _, x, y, _ = p
+        x, y = ord(x) - ord('a'), ord(y) - ord('a')
+        assert x >= 0 and x < 19 and y >= 0 and y < 19
+        game[y][x] = 1 if stone == "W" else 2
+            
+    game_out = ""
+    for y in range(len(game)):
+        row = ""
+        for x in range(len(game[y])):
+            stone = game[y][x]
+            if stone == 0:   # Empty
+                row += "."
+            elif stone == 1: # White
+                row += 'O'
+            else:            # Black
+                assert stone == 2
+                row += 'X'
+        game_out += ("%s\n" % row)
+        
+    return game_out
+
+def transform(play):
+    plays = [play]
+    def transform_impl(play, m):
+        play2 = []
+        for p in play:
+            stone, _, x, y, _ = p
+            x, y = ord(x) - ord('a'), ord(y) - ord('a')
+            assert x >= 0 and x < 19 and y >= 0 and y < 19
+            if m[0][0] != 0:
+                nx = x if m[0][0] == 1 else 18 - x
+            else:
+                nx = y if m[0][1] == 1 else 18 - y
+            if m[1][0] != 0:
+                ny = x if m[1][0] == 1 else 18 - x
+            else:
+                ny = y if m[0][1] == 1 else 18 - y
+            x, y = nx, ny
+            x, y = chr(x + ord('a')), chr(y + ord('a'))
+            play2.append("%s[%s%s]" % (stone, x, y))
+        return play2
+
+    plays.append(transform_impl(play, [[ 1,  0], [ 0, -1]]))
+    plays.append(transform_impl(play, [[-1,  0], [ 0,  1]]))
+    plays.append(transform_impl(play, [[-1,  0], [ 0, -1]]))
+    plays.append(transform_impl(play, [[ 0,  1], [ 1,  0]]))
+    plays.append(transform_impl(play, [[ 0,  1], [-1,  0]]))
+    plays.append(transform_impl(play, [[ 0, -1], [ 1,  0]]))
+    plays.append(transform_impl(play, [[ 0, -1], [-1,  0]]))
+    return plays
+
+def get_bound(play):
+    l, t, r, b = 19, 19, -1, -1
+    for p in play:
+        stone, _, x, y, _ = p
+        x, y = ord(x) - ord('a'), ord(y) - ord('a')
+        assert x >= 0 and x < 19 and y >= 0 and y < 19
+        if x < l:
+            l = x
+        if x > r:
+            r = x
+        if y < t:
+            t = y
+        if y > b:
+            b = y
+
+    return l, t, r, b
+
+def shift(play, xs, ys):
+    plays = []
+    for p in play:
+        stone, _, x, y, _ = p
+        x, y = ord(x) - ord('a'), ord(y) - ord('a')
+        assert x >= 0 and x < 19 and y >= 0 and y < 19
+        x += xs
+        y += ys
+        assert x >= 0 and x < 19 and y >= 0 and y < 19
+        x, y = chr(x + ord('a')), chr(y + ord('a'))
+        plays.append("%s[%s%s]" % (stone, x, y))
+    return plays
+
 def extract_games(game_file,
-                  num_games,
+                  out_num_games,
                   num_stones,
                   verbose):
-    unique_games, trans_unique_games, games = {}, {}, []
-    dk_games = []
+    num_games = 0
+    games, unique_games = {}, set()
     for line in game_file:
         line = line.strip()
         if line.startswith('('):
-            content = [line]
+            game_header = line
             play = ""
         else:
-            content.append(line)
             play += line
             if line.endswith(')'):
-                content = '\n'.join(content)
-                games.append(content)
+                num_games += 1
+                if num_games % 10000 == 0:
+                    print >> sys.stderr, "%d games are processed." % num_games
                 play = play[:-1]
                 play = play.split(';')[1:]
-                play = play[:num_stones]
+                orig_play = play = play[:num_stones]
+                play_str = ';'.join(play)
+                if play_str in unique_games:
+                    continue
+                unique_games.add(play_str)
 
-                def transform(play, xt, yt):
-                    play2 = []
-                    for p in play:
-                        stone, _, x, y, _ = p
-                        x, y = ord(x) - ord('a'), ord(y) - ord('a')
-                        assert x >= 0 and x < 19 and y >= 0 and y < 19
-                        if xt == 1:
-                            x = 18 - x
-                        if yt == 1:
-                            y = 18 - y
-                        x, y = chr(x + ord('a')), chr(y + ord('a'))
-                        play2.append("%s[%s%s]" % (stone, x, y))
-                    return play2
+                l, t, r, b = get_bound(play)
+                for sx in [-1, 0, 1]:
+                    if sx + l < 0 or sx + r >= 19:
+                        continue
+                    for sy in [-1, 0, 1]:
+                        if sy + t < 0 or sy + b >= 19:
+                            continue
 
-                play2 = transform(play, 1, 0)
-                play3 = transform(play, 0, 1)
-                play4 = transform(play, 1, 1)
+                        play_shift = shift(orig_play, sx, sy)
+                        play_shift_str = ';'.join(play_shift)
+                        plays = transform(play_shift)
 
-                # dk_play = set(play)
-                play = ''.join(play)
-                play2 = ''.join(play2)
-                play3 = ''.join(play3)
-                play4 = ''.join(play4)
+                        """
+                        for play in plays:
+                            play_out = draw_game(play)
+                            print play_out
+                        sys.exit(1)
+                        """
 
-                if play not in unique_games:
-                    unique_games[play] = [len(games) - 1]
-                    # dk_games.append(dk_play)
-                else:
-                    unique_games[play].append(len(games) - 1)
+                        for play in plays:
+                            play = ';'.join(play)
+                            if play in games:
+                                continue
+                            games[play] = game_header
 
-                plays = [play, play2, play3, play4]
-                plays.sort()
-                play = plays[0]
-                if play not in trans_unique_games:
-                    trans_unique_games[play] = [len(games) - 1]
-                else:
-                    trans_unique_games[play].append(len(games) - 1)
+    print >> sys.stderr, "# of games: %d, # of unique games: %d, # of reported games: %d" % (num_games, len(unique_games), len(games))
 
-    print >> sys.stderr, "# of games: %d, # of unique games: %d, # of transformed unique games: %d" % (len(games), len(unique_games), len(trans_unique_games))
-
-    game_keys = unique_games.keys()
-    random.shuffle(game_keys)
+    plays = games.keys()
+    random.shuffle(plays)
     if num_games > 0:
-        game_keys = game_keys[:num_games]
+        game_keys = plays[:out_num_games]
 
-    for game_key in game_keys:
-        assert game_key in unique_games
-        game_num = unique_games[game_key][0]
-        print games[game_num]
-
-    # DK - debugging purposes
-    """
-    dk_unique = [0 for _ in range(len(dk_games))]
-    for i in range(len(dk_games)):
-        if dk_unique[i] > 0:
-            continue
-        dk_play = dk_games[i]
-        for j in range(i+1, len(dk_games)):
-            if dk_unique[j] > 0:
-                continue
-            dk_play2 = dk_games[j]
-            if dk_play == dk_play2:
-                dk_unique[j] += 1
-
-    dk_games2 = []
-    for i in range(len(dk_unique)):
-        if dk_unique[i] > 0:
-            continue
-        dk_games2.append(dk_games[i])
-
-    print >> sys.stderr, len(dk_games2), len(dk_games)
-
-    dk_games =  dk_games2
-    dk_subset = [0 for _ in range(len(dk_games))]
-    for i in range(len(dk_games)):
-        if dk_subset[i] > 0:
-            continue
-        dk_play = dk_games[i]
-        for j in range(i+1, len(dk_games)):
-            dk_play2 = dk_games[j]
-            assert dk_play != dk_play2
-            if dk_play > dk_play2:
-                dk_subset[j] += 1
-            elif dk_play < dk_play2:
-                dk_subset[i] += 1
-                break
-
-    subset_count = 0
-    for sub in dk_subset:
-        if sub == 0:
-            subset_count += 1
-    print >> sys.stderr, "Subset count:", subset_count
-    """
-
+    for play in plays:
+        assert play in games
+        print games[play]
+        print
+        print ';%s)' % play
 
 
 if __name__ == '__main__':
